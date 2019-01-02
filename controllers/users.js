e4d7ef477b35ba354734ce26a9f9acb5be665fc5
satya_jha <satyajeetjha06@gmail.com>
2019-01-02 23:39:21 +0530

    try catch

--- controllers/users.js
+++ controllers/users.js
@@ -4,25 +4,37 @@ const User=require('../models/user');
 
 module.exports ={
 
-    
-    index:(req,res,next)=>{
-        User.find({})
-        .then (users=>{
-           res.status(200).json(users);
-        })
-        .catch(err=>{
+
+    // index:(req,res,next)=>{
+    //     User.find({})
+    //     .then (users=>{
+    //        res.status(200).json(users);
+    //     })
+    //     .catch(err=>{
+    //         next(err);
+    //     });
+    // },
+
+    index:async(req,res,next)=>{
+        try{
+            const users =await User.find({});
+            res.status(200).json(users);
+        }
+        catch(err){
             next(err);
-        });
+        }
+
     },
-    newUser:(req,res,next)=>{
-        const newUser=new User(req.body);
-        newUser.save()
-        .then(user=>{
+
+    newUser:async(req,res,next)=>{
+        try{
+            const newUser=new User(req.body);
+            const user= await newUser.save();
             res.status(201).json(user);
-        })
-        .catch(err=>{
+        }catch(err){
             next(err);
-        })
+        }
+
     }
 
 };
